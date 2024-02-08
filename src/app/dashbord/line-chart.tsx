c5a10d20763ac919ae079be92d646460df9e887c
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2024-02-08 20:24:22 +0100

    test 1

---
+++ src/app/dashbord/line-chart.tsx
@@ -0,0 +1,5 @@
+export default function Linechart(){
+  return(
+    <h1>Dashboard</h1>
+  )
+}
---
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -0,0 +1,11 @@
+export default function RewiewDetail({params}:{
+  params:{
+    productId: string
+    reviewsId: string
+  }
+}){
+  return(
+    <h1>Review {params.reviewsId} for product {params.productId}</h1>
+  )
+
+}
