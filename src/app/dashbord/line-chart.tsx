ea67a5d513ff5199e3a10e1bb36dedbe07fdf3f6
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2024-02-17 11:27:26 +0100

    add react-type, next elements

--- src/app/dashbord/line-chart.tsx
+++
@@ -1,3 +0,0 @@
-export default function Linechart() {
-  return <h1>Dashboard</h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,57 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { ReactTyped } from "react-typed";
 
 export default function Home() {
-  const question = "Will you be my valentine?";
-  const iNow = "I KNOW YOU WANT!";
-  const yesText = "jej jej jej jej, wygrałaś randkę";
-  const noText = "ejjjj nooo... :(";
-  const noText2 = "dobra jeszcze raz...";
+  const isNow = "I KNOW YOU WANT!";
+  const yesText = "yay yay yay yay";
+  const noText = "ohh nooo... :(";
+  const noText2 = "okk.. let's start again...";
 
-  const yesButtonInit = () => {
-    setTextContent(question);
+  const initTextChanges = (): void => {
+    setTextContent([noText, noText2, isNow]);
+    setTimeout(() => {
+      setVisibility(false);
+      setAdditionalClass("is-big");
+    }, 2000);
   };
 
-  const [textContent, setTextContent] = useState<string>(
-    "Will you be my valentine?",
-  );
+  const initQuestionButton = (direction: boolean): void => {
+    direction ? setTextContent([yesText]) : initTextChanges();
+  };
 
-  useEffect(() => {}, [textContent]);
+  const [textContent, setTextContent] = useState<string[]>([
+    "Will you be my valentine?",
+  ]);
+  const [isVisible, setVisibility] = useState<boolean>(true);
+  const [additionalClass, setAdditionalClass] = useState<string>("");
 
   return (
     <div className="content-holder">
       <div className="title-holder">
-        <h1>Valentine&apos;s day 💞</h1>
+        <h1>Valentine&apos;s Day 💞</h1>
       </div>
       <div className="content">
-        <p className="content-text">{textContent}</p>
+        <p className="content-text">
+          <ReactTyped strings={textContent} typeSpeed={90} />
+        </p>
+        <div className="content-image"></div>
       </div>
       <div className="buttons-holder">
-        <div onClick={yesButtonInit} className="buttons-button">
+        <div
+          onClick={() => initQuestionButton(true)}
+          className={`buttons-button ${additionalClass}`}
+        >
           TAK
         </div>
-        <div className="buttons-button">NIE</div>
+        <div
+          hidden={!isVisible}
+          onClick={() => initQuestionButton(false)}
+          className="buttons-button"
+        >
+          NIE
+        </div>
       </div>
     </div>
   );
